078d3193f5bb24a48f6f16bd30c2a48b1d18b66b
gabri <gabri@192.168.0.45>
2023-02-14 23:55:38 -0300

    inicial

---
+++ src/app/Application.java
@@ -0,0 +1,17 @@
+package app;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println(d01);
+
+	}
+
+}
